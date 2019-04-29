204b698459f2267d62acebd2a35a97a85076758f
alen1433 <alen2herceg@gmail.com>
2019-04-29 02:24:17 +0200

    made the site responsive and migrated to bootstrap

--- Scripts/teaser.js
+++ Scripts/teaser.js
@@ -1,5 +1,16 @@
 var vid = document.getElementById("smoke");
 vid.playbackRate = 1.5;
+var anim = document.getElementById("anim");
+
+myFunction();
+document.getElementsByTagName("BODY")[0].onresize = function(){myFunction()};
+
+function myFunction(){
+  if(screen.width < 576){
+    anim.style.transform = "scale(" + (screen.width/800) + ")";
+  }
+}
+
 
 setTimeout(function(){
     vid.play();
