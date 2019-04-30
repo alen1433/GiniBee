9f695cbf45e72f1b1ea23a8240f601fe6907dbb9
alen1433 <alen2herceg@gmail.com>
2019-04-30 18:18:56 +0200

    nearly finished the site,some small changes needed

--- Scripts/teaser.js
+++ Scripts/teaser.js
@@ -11,6 +11,36 @@ function myFunction(){
   }
 }
 
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'fade-in'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
 
 setTimeout(function(){
     vid.play();
